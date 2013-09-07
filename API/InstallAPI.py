c7dd534c014aa6e321f5472f289e90957f14e4de
Ryan Feng <odayfans@gmail.com>
2013-09-07 19:03:45 -0400

    Add InstallAPI

---
+++ API/InstallAPI.py
@@ -0,0 +1,6 @@
+import API.core as API
+from API.ebay import Ebay
+from API.syw import SYW
+
+API.ShoppingAPIFactory.register("newEbay", Ebay, appid='danielgu-f316-4fd2-9373-2db1b6883df2')
+API.ShoppingAPIFactory.register("newSYW", SYW, app_id=13610, app_secret='c9aabb7017d5437982680872203ce4fc', api_baseurl='http://sandboxplatform.shopyourway.com', syw_baseurl='',app_baseurl='')
