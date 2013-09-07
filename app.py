3cbbede99361e803b74b9805896238281f11d908
danielgur <danielgur2105@gmail.com>
2013-09-07 02:17:05 -0400

    init

--- app.py
+++ app.py
@@ -4,16 +4,17 @@ import simplejson as json
 import os
 
 app = Flask(__name__)
-
 api = finding(appid='danielgu-f316-4fd2-9373-2db1b6883df2')
-api.execute('findItemsAdvanced', {'keywords': 'shoes'})
 
 
-@app.route("/")
-def hello():
-      return json.dumps(api.response_dict(), sort_keys = False, indent=4)
+@app.route("/<categories>")
+def hello(categories=None):
+  if not categories:
+    return []
+  api.execute('findItemsAdvanced', {'keywords': categories})
+  return json.dumps(api.response_dict(), sort_keys = False, indent=2)
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+  port = int(os.environ.get('PORT', 5000))
+  app.run(host='0.0.0.0', port=port)
