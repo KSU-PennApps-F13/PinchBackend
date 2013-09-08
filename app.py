6731ca89330b18c4eaf5bac7c9904ee3d172fd4e
danielgur <danielgur2105@gmail.com>
2013-09-07 20:40:54 -0400

    limiting to 10 results

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ app = Flask(__name__)
 def query():
     # deny access for GET
     if request == 'GET': abort(401)
-    print str(request)
     query_dict = request.form
     try:
         kw = query_dict['kw']
